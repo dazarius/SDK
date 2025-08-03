eecb812aa19cab2496ee24ef3404cace552e5590
dazarius <merlin228mordor@gmail.com>
2025-08-03 13:01:56 +0200

    fdc

--- shadowPaySDK/__init__.py
+++ shadowPaySDK/__init__.py
@@ -1,3 +1,4 @@
-from .interface import ERC20, ERC721
-
-__all__ = ["ERC20", "ERC721", "ERC20Token"]
+from .interface.erc20 import ERC20Token
+from .interface.erc721 import ERC721Token 
+from . interface.sol import SOL
+__all__ = ["ERC20", "ERC721", "ERC20Token", "ERC721Token", "SOL"]
