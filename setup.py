eebe23f34c84871fe228b5552fc66ecea36d43ca
dazarius_ <122787233+dazarius@users.noreply.github.com>
2025-08-04 22:15:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shadowPaySDK',
-    version='1.0.7',
+    version='1.0.8',
     description='ShadowPay SDK for ERC20/ERC721 and P2P smart contract interaction',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
